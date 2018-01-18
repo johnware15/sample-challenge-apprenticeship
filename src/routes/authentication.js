268efc958a2e3e481d62193554448f1767967aca
johnware15 <contact@johndware.com>
2018-01-18 15:21:20 -0800

    Adjusted promises in routes files. Aux pages still not rendering in browser.

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,21 +1,41 @@
 import express from 'express'
 import signIn from '../actions'
 
-
 const router = express.Router()
 
-const signInRoute = router.post('/sign-in', (req, res) => {
+// const signInRoute = router.post('/sign-in', (req, res) => {
+//   const exUser = req.body
+//   signIn(req.body.email, req.body.password)
+//     .then(() => {
+//       if (exUser === true) {
+//         res.render('/')
+//       } else {
+//         // console.log('Try Again')
+//         res.render('/sign-in')
+//       }
+//     })
+//     .catch(error => error)
+// })
+//
+// module.exports = signInRoute
+
+router.get('/', (req, res, next) => {
+  signIn()
+    .then(exUser => res.render('/sign-in', {exUser}))
+    .catch(next)
+})
+
+router.get('/sign-in', (req, res, next) => {
   const exUser = req.body
   signIn(req.body.email, req.body.password)
     .then(() => {
       if (exUser === true) {
         res.render('/')
       } else {
-        // console.log('Try Again')
         res.render('/sign-in')
       }
     })
-    .catch(error => error)
+    .catch(next)
 })
 
-module.exports = signInRoute
+export default router
